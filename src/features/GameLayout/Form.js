e2230b182c47ae77067e1aa42fbadf01ceaafc7d
mosmulko <62023433@users.noreply.github.com>
2021-02-12 20:08:10 +0000

    Add mockup components for GameLayout

---
+++ src/features/GameLayout/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Label from "./Label";
+
+function Form() {
+  return (
+    <div>
+      <Label id={"x"} />
+      <Label id={"o"} />
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/features/GameLayout/Gameboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gameboard() {
+  return <div>Gameboard</div>;
+}
+
+export default Gameboard;
