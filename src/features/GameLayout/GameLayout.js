23d65ed8166a2b1ceda02a348dd16faa8cbc757b
mosmulko <62023433@users.noreply.github.com>
2021-02-12 20:04:36 +0000

    Add enum to determine the render of game's display

--- src/features/GameLayout/GameLayout.js
+++ src/features/GameLayout/GameLayout.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectStatus } from "../../app/reducers/statusSlice";
+import Form from "./Form";
+import Gameboard from "./Gameboard";
+import Winner from "./Winner";
+import Draw from "./Draw";
 
 function GameLayout() {
-  return <div>Here will be rendered game</div>;
+  const status = useSelector(selectStatus);
+
+  return (
+    <div>
+      {
+        {
+          new: <Form />,
+          playing: <Gameboard />,
+          won: <Winner />,
+          draw: <Draw />,
+        }[status]
+      }
+    </div>
+  );
 }
 
 export default GameLayout;
