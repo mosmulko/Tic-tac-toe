6dfcda3a9f4d129ea5aceb24350222de43279d7d
mosmulko <62023433@users.noreply.github.com>
2021-02-13 15:32:26 +0000

    Add form to Form Component

--- src/features/GameLayout/Form.js
+++ src/features/GameLayout/Form.js
@@ -1,11 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectStatus } from "../../app/reducers/statusSlice";
+import { selectNames } from "../../app/reducers/gameSlice";
 import Label from "./Label";
 
-function Form() {
+function Form({ submit }) {
+  const status = useSelector(selectStatus);
+  const names = useSelector(selectNames);
+  const [x, o] = names;
+
+  const [xName, setXName] = useState("");
+  const [oName, setOName] = useState("");
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = event.target.value;
+
+    target.name === "x" ? setXName(value) : setOName(value);
+  };
+
+  const handleSubmit = (event) => {
+    const newNames = [xName, oName].map((name, i) =>
+      name === "" ? (name = names[i]) : name
+    );
+    event.preventDefault();
+    submit(status, newNames);
+    setXName("");
+    setOName("");
+  };
+
   return (
     <div>
-      <Label id={"x"} />
-      <Label id={"o"} />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          <Label id={"x"} />
+          <input
+            name="x"
+            type="text"
+            placeholder={x}
+            value={xName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br></br>
+        <label>
+          <Label id={"o"} />
+          <input
+            name="o"
+            type="text"
+            placeholder={o}
+            value={oName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Start</button>
+      </form>
     </div>
   );
 }
