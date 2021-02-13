540d46e07f143f697c4ab564575d83417926f46e
mosmulko <62023433@users.noreply.github.com>
2021-02-13 15:31:26 +0000

    Create GameLayout UI render logic

--- src/features/GameLayout/GameLayout.js
+++ src/features/GameLayout/GameLayout.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { selectStatus } from "../../app/reducers/statusSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { statusChanged, selectStatus } from "../../app/reducers/statusSlice";
+import {
+  newGameStarted,
+  namesChosen,
+  selectNames,
+} from "../../app/reducers/gameSlice";
 import Form from "./Form";
 import Gameboard from "./Gameboard";
 import Winner from "./Winner";
@@ -8,17 +13,36 @@ import Draw from "./Draw";
 
 function GameLayout() {
   const status = useSelector(selectStatus);
+  const names = useSelector(selectNames);
+  const dispatch = useDispatch();
+
+  const startNewGame = (status, names) => {
+    let newStatus = status === "playing" ? "new" : "playing";
+    let [x, o] = names;
+    dispatch(newGameStarted());
+    dispatch(namesChosen({ x: x, o: o }));
+    dispatch(statusChanged(newStatus));
+  };
+
+  const handleButtonRender = (status, names) => {
+    if (status === "new") return;
+    let message = status === "playing" ? "Reset" : "Start";
+    return (
+      <button onClick={() => startNewGame(status, names)}>{message}</button>
+    );
+  };
 
   return (
     <div>
       {
         {
-          new: <Form />,
+          new: <Form submit={startNewGame} />,
           playing: <Gameboard />,
           won: <Winner />,
           draw: <Draw />,
         }[status]
       }
+      {handleButtonRender(status, names)}
     </div>
   );
 }
