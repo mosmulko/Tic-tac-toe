151ec298de629a5aabe0e68e162ec96204b78aca
mosmulko <62023433@users.noreply.github.com>
2021-02-13 15:34:02 +0000

    Add className to Label component

--- src/features/GameLayout/Label.js
+++ src/features/GameLayout/Label.js
@@ -1,8 +1,11 @@
 import React from "react";
-import "./Label.css";
 
 function Label({ id }) {
-  return <div className={id}></div>;
+  return (
+    <div className="labelContainer">
+      <div className={id}></div>
+    </div>
+  );
 }
 
 export default Label;
