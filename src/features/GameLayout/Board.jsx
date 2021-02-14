fb1f52e1d88192ee6ad2d281e271c8cbeffb6fa8
mosmulko <62023433@users.noreply.github.com>
2021-02-14 23:05:32 +0000

    Rudimentary algorithm to check the winner

--- src/features/GameLayout/Board.jsx
+++ src/features/GameLayout/Board.jsx
@@ -6,15 +6,47 @@ import {
 } from "../../app/reducers/gameSlice";
 import Field from './Field';
 
-const range = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i)
+const range = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
+
+const checkIfPlayerWon = (num, arr, player) => {
+  let fieldMap = new Map();
+  fieldMap.set(num, true);
+
+  var i = arr.indexOf(player);
+  while (i !== -1) {
+    fieldMap.set(i, true);
+  i = arr.indexOf(player, i + 1);
+  }
+
+  console.log(fieldMap.keys());
+  
+  if (
+    (fieldMap.has(1) && fieldMap.has(2) && fieldMap.has(3)) ||
+    (fieldMap.has(4) && fieldMap.has(5) && fieldMap.has(6)) ||
+    (fieldMap.has(7) && fieldMap.has(8) && fieldMap.has(9)) ||
+    (fieldMap.has(1) && fieldMap.has(4) && fieldMap.has(7)) ||
+    (fieldMap.has(2) && fieldMap.has(5) && fieldMap.has(8)) ||
+    (fieldMap.has(3) && fieldMap.has(6) && fieldMap.has(9)) ||
+    (fieldMap.has(1) && fieldMap.has(5) && fieldMap.has(9)) ||
+    (fieldMap.has(3) && fieldMap.has(5) && fieldMap.has(7))
+  ) return true;
+  return false;
+}
 
 function Board({player}) {
   const dispatch = useDispatch();
   const [statuses, setStatuses] = useState([]);
   const [availableFields, setAvailableFields] = useState(9);
+  const [winner, setWinner] = useState(null);
 
   useEffect(() => {
+    if (winner) {
+      console.log('winner!')
+      return dispatch(statusChanged('won'))
+    } 
+    
     if (availableFields === 0) {
+      console.log('it"s a tie!')
       dispatch(statusChanged('draw'));
     }
   })
@@ -25,10 +57,14 @@ function Board({player}) {
 
   const markField = (num, player) => {
     if (statuses[num] !== undefined) return;
-    var newStatuses = [...statuses];
-    newStatuses[num] = player;
-    setStatuses(newStatuses);
-    setAvailableFields(availableFields - 1);
+    if (checkIfPlayerWon(num, statuses, player)) {
+      setWinner(player)
+    } else {
+      const newStatuses = [...statuses];
+      newStatuses[num] = player;
+      setStatuses(newStatuses);
+      setAvailableFields(availableFields - 1);
+    }
     dispatch(playerTurnEnded());
   }
 
