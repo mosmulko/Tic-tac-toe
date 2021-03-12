3cfc15364ee53e3f46851bdb897100ffb9da7463
mosmulko <62023433@users.noreply.github.com>
2021-03-12 14:22:11 +0100

    Add getWinningCombinationsFor function

---
+++ src/utils/getWinningCombinationsFor.js
@@ -0,0 +1,45 @@
+export default function getWinningCombinationsFor(field) {
+  const row = field[0];
+  const col = field[1];
+  const winningPairsArray = [];
+
+  // loop that gets matches based on a row
+  {
+    let winningPair = [];
+    for (let i = 0; i < 3; i++) {
+      if (i === +col) continue;
+      winningPair.push(`${row}${i}`);
+    }
+    winningPairsArray.push(winningPair);
+  }
+
+  //loop that gets matches based on a column
+  {
+    let winningPair = [];
+    for (let i = 0; i < 3; i++) {
+      if (i === +row) continue;
+      winningPair.push(`${i}${col}`);
+    }
+    winningPairsArray.push(winningPair);
+  }
+
+  if (row === col) {
+    let winningPair = [];
+    for (let i = 0; i < 3; i++) {
+      if (i === +row) continue;
+      winningPair.push(`${i}${i}`);
+    }
+    winningPairsArray.push(winningPair);
+  }
+
+  if (+row + +col === 2) {
+    let winningPair = [];
+    for (let i = 0, j = 2; i < 3; i++, j--) {
+      if (i === +row) continue;
+      winningPair.push(`${i}${j}`);
+    }
+    winningPairsArray.push(winningPair);
+  }
+
+  return winningPairsArray;
+}
