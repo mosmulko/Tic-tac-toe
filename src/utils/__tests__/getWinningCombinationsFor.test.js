7f7bb5d46d0234f8c84d54b25fb158d45f91dd52
mosmulko <62023433@users.noreply.github.com>
2021-03-12 14:48:20 +0100

    Refactor getWinningCombinationsFor.test
    Adds edge cases specs

--- src/utils/__tests__/getWinningCombinationsFor.test.js
+++ src/utils/__tests__/getWinningCombinationsFor.test.js
@@ -21,4 +21,25 @@ describe("getWinningCombinationsFor", () => {
     let actual = getWinningCombinationsFor("11");
     expect(actual).toEqual(expected);
   });
+
+  it("returns an array of possible winning matches for the field '10'", function () {
+    let expected = [
+      ["11", "12"],
+      ["00", "20"],
+    ];
+    let actual = getWinningCombinationsFor("10");
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns an empty array if no field is passed", function () {
+    let expected = [];
+    let actual = getWinningCombinationsFor();
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns an empty array if nonexistent field is passed", function () {
+    let expected = [];
+    let actual = getWinningCombinationsFor("31");
+    expect(actual).toEqual(expected);
+  });
 });
